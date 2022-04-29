3287604e96dfc35f60f2934592f5febef9e5cfff
GodKimba <rafaelfadel2@gmail.com>
2022-04-29 08:00:42 -0300

    finished the hello_test, again...

--- hello_test.go
+++ hello_test.go
@@ -3,21 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	assertCorrectMessage := func (t testing.TB, got, want string)  {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	}
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Rafa")
-		want := "Hello, Rafa"
-		assertCorrectMessage(t, got, want)
+	got := Hello("Rafa")
+	want := "Hello, Rafa"
 
-	})
-	t.Run("say, 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
-		assertCorrectMessage(t, got, want)
-	})
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
