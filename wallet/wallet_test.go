8ab79cc7264a152b17989605e11f779bdf039dcb
GodKimba <rafaelfadel2@gmail.com>
2022-05-01 09:21:08 -0300

    restarted this section, as it's not all that clear to me how interfaces in golang work

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"testing"
-	"fmt"
 )
 
-func TestWaller(t *testing.T) {
+func TestWallet(t *testing.T) {
 
 	wallet := Wallet{}
 
 	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-
-	fmt.Printf("address of balance in test if %v \n", &wallet.balance)
 
 	want := Bitcoin(10)
 
