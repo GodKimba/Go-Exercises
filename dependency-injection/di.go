a056c41d35d1a44471301d074ab5627eeb3618de
GodKimba <rafaelfadel2@gmail.com>
2022-05-02 09:09:59 -0300

    finished the dependency injection exercise

--- dependency-injection/di.go
+++ dependency-injection/di.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-  "fmt"
-  "bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-func Greet( writer *bytes.Buffer, name string) string {
-  fmt.Fprintf(writer, "Hello, %s", name)
-  return name
+func Greet(writer io.Writer, name string) string {
+	fmt.Fprintf(writer, "Hello, %s", name)
+	return name
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
+}
