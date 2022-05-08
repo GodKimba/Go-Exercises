951a798c71f9da9ba231fc25a6c9da80fd06995a
GodKimba <rafaelfadel2@gmail.com>
2022-05-08 12:58:03 -0300

    added some more tests

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,10 +3,32 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-  val := reflect.ValueOf(x) 
-  
-  for i := 0; i < val.NumField(); i++ {
-    field := val.Field(i)
-    fn(field.String())
-  }
+	val := getvalue(x)
+	
+	if val.Kind() == reflect.Slice {
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+		return
+	}
+	
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+	}
+}
+
+func getvalue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
 }
