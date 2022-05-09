b8a34f6ef314470f05b0e3ffee4d95bafde4c007
GodKimba <rafaelfadel2@gmail.com>
2022-05-09 09:22:31 -0300

    finished the sync exercise

--- sync/sync.go
+++ sync/sync.go
@@ -1,12 +1,18 @@
 package testing
 
+import (
+	"sync"
+)
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
-
 }
 
 func (c *Counter) Value() int {
