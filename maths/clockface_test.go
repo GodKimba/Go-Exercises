2ab6381c34d24bebe89185173134bdbbda43b0af
GodKimba <rafaelfadel2@gmail.com>
2022-05-30 09:27:01 -0300

    added the clockface exercise

--- maths/clockface_test.go
+++ maths/clockface_test.go
@@ -1,30 +1,66 @@
-package clockface_test
+package clockface
 
 import (
+	"math"
 	"testing"
 	"time"
-
-	"github.com/quii/learn-go-with-tests/math/v1/clockface"
+	//"github.com/quii/learn-go-with-tests/math/v1/clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+// func TestSecondHandAtMidnight(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+// 	want := clockface.Point{X: 150, Y: 150 - 90}
+// 	got := clockface.SecondHand(tm)
 
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
+// 	if got != want {
+// 		t.Errorf("Got %v, wanted %v", got, want)
+// 	}
+// }
+
+// func TestSecondHandAt30Seconds(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+// 	want := clockface.Point{X: 150, Y: 150 + 90}
+// 	got := clockface.SecondHand(tm)
+
+// 	if got != want {
+// 		t.Errorf("Got %v, wanted %v", got, want)
+// 	}
+
+func TestSecondsInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
+
+	if want != got {
+		t.Fatalf("Wanted %v radians, but got %v", want, got)
 	}
 }
 
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
 
-	want := clockface.Point{X: 150, Y: 150 + 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
 }
